cefde60c9ee6a8a86b033a5dc4df3d60f3784173
estrazulas <daniel.estrazulas@gmail.com>
2026-04-29 17:32:21 -0300

    chore: initial commit — make project standalone

---
+++ src/graph/nodes/chatNode.ts
@@ -0,0 +1,35 @@
+import type { GraphState } from '../state.ts';
+import { AIMessage } from '@langchain/core/messages';
+import { OpenRouterService } from '../../services/openrouterService.ts';
+import { prompts } from '../../config.ts';
+import { PromptTemplate } from '@langchain/core/prompts';
+
+export const createChatNode = (openRouterService: OpenRouterService) => {
+    return async (state: GraphState): Promise<Partial<GraphState>> => {
+        try {
+            const userPrompt = state.messages?.[state.messages.length - 1]?.text || "";
+            const template = PromptTemplate.fromTemplate(prompts.system);
+            const systemPrompt = await template.format({ 
+                USER_ROLE: state.user.role,
+                USER_NAME: state.user.displayName
+            });
+                        // exemplo abaixo é mais inseguro!!
+            // const systemPrompt = prompts.system
+            //  .replace('{USER_ROLE}', state.user.role);
+            //  .replace('{USER_NAME}', state.user.displayName);
+
+            const respose = await openRouterService.generate(systemPrompt, userPrompt);
+
+
+
+            return {
+                messages: [new AIMessage(respose)],
+            };
+        } catch (error) {
+            console.error('Chat node error:', error);
+            return {
+                messages: [new AIMessage('I apologize, but I encountered an error processing your request. Please try again later.')],
+            };
+        }
+    }
+}
